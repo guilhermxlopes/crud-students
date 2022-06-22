0cb2d29e737d7e58c1637343f347fe55f1ab95fb
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-06-22 14:52:20 -0300

    Create function

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const getLocalStorage = () => JSON.parse(localStorage.getItem("data_student")) ?? []; // get data from the localStorage
 const setLocalStorage = (dataStudent) => localStorage.setItem("data_student", JSON.stringify(dataStudent)); // set data to the localStorage
+
+const studentTemp = {
+  name: "Guilherme",
+  grr: "20000102",
+  birthdate: "20/01/1990",
+  email: "guilhermxlopes@gmail.com",
+};
+
+// CREATE STUDENT
+const createStudent = (student) => {
+  const dataStudent = getLocalStorage();
+  dataStudent.push(student);
+  setLocalStorage(dataStudent);
+};
