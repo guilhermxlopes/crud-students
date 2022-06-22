adab5262f1de3faf65e951f8bdc9314483b4d765
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-06-22 15:35:14 -0300

    Integration with forms layout

--- script.js
+++ script.js
@@ -2,15 +2,40 @@ const getLocalStorage = () => JSON.parse(localStorage.getItem("data_student")) ?
 const setLocalStorage = (dataStudent) => localStorage.setItem("data_student", JSON.stringify(dataStudent)); // set data to the localStorage
 
 const studentTemp = {
-  name: "Guilherme",
+  name: "Guiaaalherme",
   grr: "20000102",
   birthdate: "20/01/1990",
   email: "guilhermxlopes@gmail.com",
 };
 
-// CREATE STUDENT
+///////////////////////////////// CREATE STUDENT
 const createStudent = (student) => {
   const dataStudent = getLocalStorage();
   dataStudent.push(student);
   setLocalStorage(dataStudent);
 };
+
+// CHECK IF FORMS IS COMPLETED
+const isCompletedFields = () => {
+  return document.querySelector("form").reportValidity(); // retorna verdadeiro/true com todos os requisitos do HTML cumpridos
+};
+
+// SAVE STUDENT
+const saveStudent = () => {
+  if (isCompletedFields()) {
+    const student = {
+      name: document.getElementById("f-name").value,
+      gr: document.getElementById("f-gr").value,
+      birthdate: document.getElementById("f-birthdate").value,
+      email: document.getElementById("f-email").value,
+    };
+    const index = document.getElementById("f-name").dataset.index;
+    if (index == "new") {
+      createStudent(student);
+    }
+  }
+};
+
+// BUTTON TRIGGER
+document.getElementById("submit").addEventListener("click", saveStudent);
+document.getElementById("submit").addEventListener("click", console.log(saveStudent));
